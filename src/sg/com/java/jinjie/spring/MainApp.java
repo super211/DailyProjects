c4c73396293f211f664f50d282c2bf046062c435
Bruce Liu <bruce.liu@tdsecurities.com>
2016-09-07 18:38:53 +0800

    Event Handling in Spring

--- src/sg/com/java/jinjie/spring/MainApp.java
+++ src/sg/com/java/jinjie/spring/MainApp.java
@@ -1,20 +1,25 @@
 package sg.com.java.jinjie.spring;
 
-//http://www.tutorialspoint.com/spring/spring_bean_scopes.htm Spring - Bean Scopes
-
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		//ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		context.start(); //This method needs ConfigurableApplicationContext
 		HelloWorld objA = (HelloWorld)context.getBean("helloWorld");
 		objA.setMessage("I am object A"); //I am object A
 		System.out.println(objA.getMessage());
 		
+		//context.refresh(); // the instance of singleton bean 'helloWorld' will be destroyed and recreated.
+		
 		HelloWorld objB = (HelloWorld)context.getBean("helloWorld");
 		System.out.println(objB.getMessage()); //I am object A
+		
+		context.stop();
+		context.close();
 	}
 
 }
